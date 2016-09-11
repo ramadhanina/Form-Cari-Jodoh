f1da2adfa23eff40c6bcb22bac980e35814d7e29
Nina Aulia Ramadhani <ninshanni@gmail.com>
2016-09-11 17:12:11 +0700

    +imageview hati +textViewjudul +textviewnama +editTextnama +spinner for show experience +textviewjeniskelamin +radiogrup for jenis kelamin +radiobutton wanita +radio button pria +textview tipe pasangan +checkbox tipe - tipe pasangan +button Tampilkan +MediumTextHasil +textView Usia +editTextUsia

--- app/src/main/java/id/sch/smktelkom_mlg/learn/formcarijodoh/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/formcarijodoh/MainActivity.java
@@ -2,12 +2,20 @@ package id.sch.smktelkom_mlg.learn.formcarijodoh;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama, etUsia;
+
+    Button bTampilkan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextnama);
+        etUsia = (EditText) findViewById(R.id.editTextUsia)
     }
 }

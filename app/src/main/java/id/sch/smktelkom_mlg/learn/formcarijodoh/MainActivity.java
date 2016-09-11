5497e8a8e0b8984707238bc5f83e5658d3e7afcf
Nina Aulia Ramadhani <ninshanni@gmail.com>
2016-09-11 17:36:11 +0700

    +imageview hati +textViewjudul +textviewnama +editTextnama +spinner for show experience +textviewjeniskelamin +radiogrup for jenis kelamin +radiobutton wanita +radio button pria +textview tipe pasangan +checkbox tipe - tipe pasangan +button Tampilkan +textView Usia +editTextUsia +TextView with medium size

--- app/src/main/java/id/sch/smktelkom_mlg/learn/formcarijodoh/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/formcarijodoh/MainActivity.java
@@ -2,11 +2,14 @@ package id.sch.smktelkom_mlg.learn.formcarijodoh;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etNama, etUsia;
+    TextView tvHasil;
 
     Button bTampilkan;
 
@@ -16,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etNama = (EditText) findViewById(R.id.editTextNama);
-        etUsia = (EditText) findViewById(R.id.editTextUsia)
+        etUsia = (EditText) findViewById(R.id.editTextUsia);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        bTampilkan.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              String Nama = etNama.getText().toString();
+                                              String Usia = etUsia.getText().toString();
+                                              tvHasil.setText("Saya " + Nama + " berusia " + Usia + "tahun");
+
+                                          }
+                                      }
+        );
     }
 }
+

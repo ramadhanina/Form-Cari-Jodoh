71120f3a2a00889460373dc9208cf09f536f03b0
Nina Aulia Ramadhani <ninshanni@gmail.com>
2016-09-11 21:35:19 +0700

    +imageview hati +textViewjudul +textviewnama +editTextnama +spinner for show experience +textviewjeniskelamin +radiogrup for jenis kelamin +radiobutton wanita +radio button pria +textview tipe pasangan +checkbox tipe - tipe pasangan +button Tampilkan +textView Usia +editTextUsia +TextView with medium size

--- app/src/main/java/id/sch/smktelkom_mlg/learn/formcarijodoh/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/formcarijodoh/MainActivity.java
@@ -3,18 +3,25 @@ package id.sch.smktelkom_mlg.learn.formcarijodoh;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etNama, etUsia;
     TextView tvHasil;
     RadioButton rbWanita, rbPria;
-
+    RadioGroup rgGender;
+    CheckBox cbRomantis, cbAlim, cbHumoris, cbSetia, cbSerius, cbBerkarir;
+    Spinner spPengalaman;
     Button bTampilkan;
 
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
         rbWanita = (RadioButton) findViewById(R.id.radioButtonWanita);
         rbPria = (RadioButton) findViewById(R.id.radioButtonPria);
+        rgGender = (RadioGroup) findViewById(R.id.radioGroupGender);
+        cbRomantis = (CheckBox) findViewById(R.id.checkBoxRomantis);
+        cbAlim = (CheckBox) findViewById(R.id.checkBoxAlim);
+        cbHumoris = (CheckBox) findViewById(R.id.checkBoxHumoris);
+        cbSetia = (CheckBox) findViewById(R.id.checkBoxSetia);
+        cbSerius = (CheckBox) findViewById(R.id.checkBoxSerius);
+        cbBerkarir = (CheckBox) findViewById(R.id.checkBoxBerkarir);
+        spPengalaman = (Spinner) findViewById(R.id.spinnerpengalaman);
+        )
 
         bTampilkan.setOnClickListener(new View.OnClickListener() {
                                           @Override
@@ -40,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
             String Nama = etNama.getText().toString();
             String Usia = etUsia.getText().toString();
             String gender = null;
-            tvHasil.setText("Saya " + Nama + " berusia " + Usia + "tahun." + "Saya adalah seorang" + gender + "tulen");
+            String tipe = "Tipe Pasangan Saya adalah ";
+            tvHasil.setText("Saya " + Nama + " berusia " + Usia + "tahun." + "Saya adalah seorang" + gender + "tulen, " + tipe + ".Pengalaman pahit yang pernah saya alami adalah" + spPengalaman.getSelectedItem().toString());
         }
 }
     private boolean isValid() {
@@ -48,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
         String Nama = etNama.getText().toString();
         String Usia = etUsia.getText().toString();
         String gender = null;
+        String tipe = "Tipe Pasangan Saya adalah ";
+        int startlen = tipe.length();
 
         if (Nama.isEmpty()) {
             etNama.setError("Nama belum diisi");
@@ -68,16 +87,25 @@ public class MainActivity extends AppCompatActivity {
             etUsia.setError(null);
         }
 
-        if (rbWanita.isChecked()) {
-            gender = rbWanita.getText().toString();
-        } else if (rbPria.isChecked()) {
-            gender = rbPria.getText().toString();
+        if (rgGender.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton)
+                    findViewById(rgGender.getCheckedRadioButtonId());
+            gender = rb.getText().toString();
         }
         if (gender == null) {
             tvHasil.setText("Anda belum menentukan gender");
         } else {
             tvHasil.setText(" Saya adalah seorang " + gender);
         }
+        if (cbRomantis.isChecked()) tipe += cbRomantis.getText();
+        if (cbAlim.isChecked()) tipe += cbAlim.getText();
+        if (cbHumoris.isChecked()) tipe += cbHumoris.getText();
+        if (cbSetia.isChecked()) tipe += cbSetia.getText();
+        if (cbSerius.isChecked()) tipe += cbSerius.getText();
+        if (cbBerkarir.isChecked()) tipe += cbBerkarir.getText();
+
+        if (tipe.length() == startlen) tipe += "Tidak ada pada Pilihan";
+        tvHasil.setText(tipe);
         return valid;
     }
 }
